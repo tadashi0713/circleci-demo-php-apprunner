c4787e767e746a0f7bb3b4ccdc92aba3707828ac
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-10-20 16:14:06 +0900

    Add lint

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseMigrations;
-use Laravel\Lumen\Testing\DatabaseTransactions;
+// use Laravel\Lumen\Testing\DatabaseMigrations;
+// use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
